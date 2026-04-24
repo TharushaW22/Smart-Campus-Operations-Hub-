cf4ab6a73b40fab379d84f5c499ff49af4b8818c
SashiniWickramarathne <sbwicky910@gmail.com>
2026-04-24 10:36:46 +0530

    Update ResourceController.java

--- back-end/src/main/java/com/smartcampus/back_end/controller/ResourceController.java
+++ back-end/src/main/java/com/smartcampus/back_end/controller/ResourceController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/resources")
+@RequestMapping("/api/resources")  //endpoint for resource management
 @CrossOrigin(origins = "*")
 public class ResourceController {
 
